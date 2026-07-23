408f45dfac949cae3259dec055c459208b072d39
alexoxy <alex@oxygencapital.net>
2026-07-23 21:57:37 +0100

    Centralize production site origin

---
+++ lib/site.ts
@@ -0,0 +1,3 @@
+export const SITE_ORIGIN = 'https://www.santosdodia.com' as const;
+export const SITE_HOSTNAME = 'www.santosdodia.com' as const;
+export const APEX_HOSTNAME = 'santosdodia.com' as const;
