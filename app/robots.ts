9ea8f057ac6692b2164152a947c1a0f73e500977
alexoxy <alex@oxygencapital.net>
2026-08-02 17:46:52 +0100

    Keep operational system endpoints out of search indexes

--- app/robots.ts
+++ app/robots.ts
@@ -3,7 +3,11 @@ import { SITE_ORIGIN } from '../lib/site';
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: { userAgent: '*', allow: '/', disallow: ['/api/v1/context'] },
+    rules: {
+      userAgent: '*',
+      allow: '/',
+      disallow: ['/api/v1/context', '/api/v1/system/']
+    },
     sitemap: `${SITE_ORIGIN}/sitemap.xml`,
     host: SITE_ORIGIN
   };
